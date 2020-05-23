54f8853b0722f74c9689bcd4daa89c718ee2d0d0
Jizu Sun <jizu.sun@sap.com>
2020-05-23 19:49:05 +0800

    use rescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Example from './Example'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Example />
         <a
           className="App-link"
           href="https://reactjs.org"
